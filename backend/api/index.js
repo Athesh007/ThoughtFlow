fed8a7182ad620701d4a5d10cbdc3fe7e84e0734
Athesh007 <kavipprasath80@gmail.com>
2024-08-25 18:40:09 +0530

    vercel

---
+++ backend/api/index.js
@@ -0,0 +1,24 @@
+import cors from "cors";
+import express from "express";
+import router from "../router/posts.js";
+import userrouter from "../router/users.js";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = await mongoose.connect(process.env.MONGODB_URI);
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.use("/posts", router);
+app.use("/user", userrouter);
+
+app.listen(3000, () => {
+  console.log(`Listening on port 3000`);
+});
+
+export default db;
